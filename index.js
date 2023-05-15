9b4b661ef8eae6e744e323b5a68f0e845e0482da
jvgames-BR16 <85315268+jvgames-BR16@users.noreply.github.com>
2023-05-14 23:07:28 -0300

    wha

---
+++ index.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("butt");
+button.addEventListener("click", access());
+
+       function access(){
+            var inputVal = document.getElementById("FreeDiamondsOverHere!").value;
+            alert(inputVal);
+            
+        }
